409d4a6887fcf94f555d2630b3dc17c60f33625b
Andrew Skelly <andrewskelly@hotmail.co.uk>
2026-01-28 13:49:51 +0000

    Fix target group targeting type for ECS deployment
    - Change target_type from 'ip' to 'instance' to match existing infrastructure
    - Prevents target group recreation during deployment
    - Ensures compatibility with 1 EC2 instance + 2 tasks architecture

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -125,7 +125,7 @@ resource "aws_lb_target_group" "main" {
   port        = 3000
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
-  target_type = "ip"
+  target_type = "instance"
 
   health_check {
     enabled             = true
