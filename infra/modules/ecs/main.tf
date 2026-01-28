280dac499ba53753e099db75c0a787fa99c653db
Andrew Skelly <andrewskelly@hotmail.co.uk>
2026-01-28 13:51:42 +0000

    Fix target group compatibility with awsvpc network mode
    - Change target_type back to 'ip' to match awsvpc network mode requirements
    - Removed target group from Terraform state to allow recreation
    - Ensures ECS service creation compatibility with 2 tasks on 1 EC2 instance

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -125,7 +125,7 @@ resource "aws_lb_target_group" "main" {
   port        = 3000
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
-  target_type = "instance"
+  target_type = "ip"
 
   health_check {
     enabled             = true
